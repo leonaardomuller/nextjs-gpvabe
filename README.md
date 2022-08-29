7d5ac7708f6b1a1fe4c74ff73552d02ee5ac80c4
leonaardomuller <leonaardo.muller@gmail.com>
2022-08-29 18:28:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-gpvabe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-gpvabe)
---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { GetStaticProps } from 'next';
+
+export default function Home({ repositories, date }) {
+  useEffect(() => {}, []);
+  return (
+    <>
+      <h1>{date}</h1>
+      <ul>
+        {repositories.map((repo) => (
+          <li key={repo}>{repo}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch(
+    'https://api.github.com/users/leonaardomuller/repos'
+  );
+  const data = await response.json();
+  const repositoriesNames = data.map((repo) => repo.name);
+
+  return {
+    props: {
+      repositories: repositoriesNames,
+      date: new Date().toISOString(),
+    },
+    revalidate: 5,
+  };
+};
