cf9be20b5e89718f81ff91d9abb87c8caf52ae05
leonaardomuller <leonaardo.muller@gmail.com>
2022-08-30 12:15:04 -0300

    2 files added

---
+++ pages/blog/middleware.ts
@@ -0,0 +1,10 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.rewrite(
+    'https://nextjs-gpvabe--3000.local.webcontainer.io/'
+  );
+}
