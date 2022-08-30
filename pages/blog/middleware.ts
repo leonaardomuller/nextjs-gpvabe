ed3fccb5ce1fbc6cfc79d5d95d69de5977a52d77
leonaardomuller <leonaardo.muller@gmail.com>
2022-08-30 12:21:54 -0300

    1 file modified, 1 deleted

--- pages/blog/middleware.ts
+++ pages/blog/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.rewrite(
+  return NextResponse.redirect(
     'https://nextjs-gpvabe--3000.local.webcontainer.io/'
   );
 }
